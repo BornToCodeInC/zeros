378d4d48ae889568be7b0fd6d385ea334e9cc15c
Victoria <emilytoyacc@gmail.com>
2018-03-05 17:11:22 +0300

    zeros/feature: Implement getZerosCount function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+    let zeroCount = 0;
+    while(number > 0) {
+        number = Math.floor(number  / 5);
+        zeroCount += number;
+    }
+    return zeroCount;
+};
